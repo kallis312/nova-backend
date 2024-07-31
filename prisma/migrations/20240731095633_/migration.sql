b739af144f13512e6ad566a0aa4a6f0efb029c43
anhnt1-wano <anhnt1@wanosoft.com>
2024-07-31 17:03:42 +0700

    fix migration

--- prisma/migrations/20240731095633_/migration.sql
+++ prisma/migrations/20240731095633_/migration.sql
@@ -1,5 +1,13 @@
--- AlterTable
-ALTER TABLE `User` ADD COLUMN `role` ENUM('ADMIN', 'USER') NULL DEFAULT 'USER';
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `role` ENUM('ADMIN', 'USER') NULL DEFAULT 'USER',
+
+    UNIQUE INDEX `User_username_key`(`username`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `Dicom` (
