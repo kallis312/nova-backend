7d5193e7bf0626acaf36efb37aaa9e3614835795
creativeDev0508 <mingw2816@gmail.com>
2024-08-08 16:44:32 +0900

    update integrate with jupiter

--- src/controllers/dicomController.ts
+++ src/controllers/dicomController.ts
@@ -1,5 +1,7 @@
+import { jupiterServer } from '@/config/axiosConfig';
 import prisma from '@/config/dbConfig';
 import { dicomAnnotationVallidator } from '@/validators/dicomValidator';
+import { Dicom } from '@prisma/client';
 import { NextFunction, Request, Response } from 'express';
 
 const perPage = 10;
@@ -27,13 +29,25 @@ export const getDicomList = async (req: Request, res: Response, next: NextFuncti
 export const getDicom = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { id } = req.params
-    const _dicoms = await prisma.dicom.findUnique({
+    
+    const existDicom = await prisma.dicom.findUnique({
       where: { id },
     })
-    // Download Dicom data usging dicoms.id from Jupiter Server
-    res.json({
-      message: 'Downloading...'
+    if (!existDicom) throw new Error('No dicom found');
+    
+    const response = await jupiterServer.get('/dicom/' + id, {
+      responseType: 'stream'
     });
+
+    // Download Dicom data usging dicoms.id from Jupiter Server
+
+    res.setHeader('Content-Disposition', `attachment; filename="${id}.zip"`);
+    res.setHeader('Content-Type', 'application/zip');
+
+    response.data.pipe(res);
+    response.data.on('error', (err: Error) => {
+      throw new Error(err.message);
+    })
   } catch (error) {
     next(error)
   }
@@ -103,6 +117,31 @@ export const actionAnnotation = async (req: Request, res: Response, next: NextFu
       })
     }
     res.json(resultDicomAnnotation);
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const getDicomFromJupiter = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const jupiterDicoms = await jupiterServer.get<Dicom[]>('/dicoms');
+    await prisma.dicom.deleteMany({
+      where: {
+        id: {
+          notIn: jupiterDicoms.data.map(dicom => dicom.id)
+        }
+      }
+    })
+    const result = await Promise.all(jupiterDicoms.data.map(async dicom => {
+      return await prisma.dicom.upsert({
+        where: {
+          id: dicom.id
+        },
+        create: dicom,
+        update: dicom
+      })
+    }))
+    res.json(result);
   } catch (error) {
     next(error)
   }
