c083612ac879c7563850033dae9f713cab137c24
anhnt1-wano <anhnt1@wanosoft.com>
2024-08-13 14:28:50 +0700

    feat(admin): review-dicom

--- src/controllers/admin/dicomController.ts
+++ src/controllers/admin/dicomController.ts
@@ -1,10 +1,15 @@
+import { reviewDicomService } from '@/services/dicomService';
+import { reviewDicomValidator } from '@/validators/dicomValidator';
 import { NextFunction, Request, Response } from 'express';
 
 export const dicomReview = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const result: string[] = []
-    res.json(result);
+    const { id } = req.params;
+    const body = reviewDicomValidator.parse(req.body);
+    await reviewDicomService(id, body);
+
+    return res.json({ message: "Successfully" });
   } catch (error) {
-    next(error)
+    next(error);
   }
 }
