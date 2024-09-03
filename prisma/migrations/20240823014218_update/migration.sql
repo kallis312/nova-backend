c1def3b927e2e13cd4ef7b8ddca05c7748943e8c
creativeDev0508 <mingw2816@gmail.com>
2024-09-03 11:59:47 +0900

    re migrate

--- prisma/migrations/20240823014218_update/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE `dicom` ADD COLUMN `review` ENUM('accept', 'pending', 'reject') NULL,
-    ADD COLUMN `reviewBy` VARCHAR(191) NULL;
--- prisma/migrations/20240823020237_review_update/migration.sql
+++
@@ -1,2 +0,0 @@
--- AddForeignKey
-ALTER TABLE `Dicom` ADD CONSTRAINT `Dicom_reviewBy_fkey` FOREIGN KEY (`reviewBy`) REFERENCES `User`(`username`) ON DELETE CASCADE ON UPDATE CASCADE;
